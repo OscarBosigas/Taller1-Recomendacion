143e276c87c0689c5db60169bedc769732d266a6
Oscar Bosigas <oscar7bosigas@gmail.com>
2024-03-12 00:36:28 -0500

    Recomendaciones con canciones

--- Back/flaskr/vistas/vistas.py
+++ Back/flaskr/vistas/vistas.py
@@ -44,7 +44,7 @@ class Usuario(Resource):
         
 class Predict(Resource):
     def post(self):
+        
         df = pd.read_csv('modelos/predictions.csv')
-        result = df.loc[df['userid'] == request.json['id']]
-        print(json.loads(result.to_json(orient='records')))
+        result = df.loc[df['userid'] == request.json['uid']]
         return json.loads(result.to_json(orient='records'))
