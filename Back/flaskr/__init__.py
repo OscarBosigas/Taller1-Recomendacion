78f7c115d3614a9d65c6cbc45676c73bfb6c0a8b
Oscar Bosigas <oscar7bosigas@gmail.com>
2024-03-11 23:50:23 -0500

    .

--- Back/flaskr/__init__.py
+++ Back/flaskr/__init__.py
@@ -5,4 +5,5 @@ def create_app(config_name):
     app.config['PROPAGATE_EXCEPTIONS']=True
     app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://u2m45vs8bivl7h:p82765ac638851a2a57befbb9c6f734646e32b33794ade80ddb194b3b64ca7cda@ceu9lmqblp8t3q.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dcuqbml40smda4'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    
     return app
