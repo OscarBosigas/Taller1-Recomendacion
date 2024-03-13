1e5541f6d4d5520f8bb5ae1a729dbf4a4ba4994a
Oscar Bosigas <oscar7bosigas@gmail.com>
2024-03-13 02:34:10 -0500

    validaciones front

--- Front/src/app/recomendacion/recomendacion.component.ts
+++ Front/src/app/recomendacion/recomendacion.component.ts
@@ -27,6 +27,28 @@ Object: any;
     this.artistaService.getRecomendaciones(new Usuario(this.id,'','','','','',0)).subscribe(res =>{
       this.artistas = res
       this.datos = res[0].top_songs_by_artist;
+      this.datos = this.datos.replace(/"/g, "'");
+      this.datos = this.datos.replace("0'", "0");
+      this.datos = this.datos.replace("I'M", "IM");
+      this.datos = this.datos.replace(/'']/g, "']");
+      this.datos = this.datos.replace(/\\/g, "");
+      this.datos = this.datos.replace(/'s/g, "s");
+      this.datos = this.datos.replace(/t'S/g, "S");
+      this.datos = this.datos.replace(/n'T/g, "nT");
+      this.datos = this.datos.replace(/n't/g, "nt");
+      this.datos = this.datos.replace(/´/g, "");
+      this.datos = this.datos.replace(/`/g, "");
+      this.datos = this.datos.replace(/1'/g, "1 ");
+      this.datos = this.datos.replace(/g'll/g, "ll");
+      this.datos = this.datos.replace(/g'Ll/g, "Ll");
+      this.datos = this.datos.replace(/'Ll/g, "Ll");
+      this.datos = this.datos.replace("!", "");
+      console.log(this.datos)
+      
+      console.log(this.datos.charAt(216))
+      console.log(this.datos.charAt(217))
+      console.log(this.datos.charAt(218))
+      console.log(this.datos.charAt(219))
       this.datos = JSON.parse(this.datos.replace(/'/g, '"'));
       this.nombres = Object.keys(this.datos)
     })
